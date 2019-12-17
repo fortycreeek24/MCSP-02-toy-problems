ba94521727a11670b60cfcb4340409111a8e2bb8
Noah Smith <fortycreeek24@yahoo.com>
2019-12-17 10:01:56 -0600

    finish toy problem

--- balancedBrackets/prompt.js
+++ balancedBrackets/prompt.js
@@ -9,4 +9,37 @@
 // str: "[{()}]"                  |   true
 // str: "(50)("                   |   false
 // str: "[{]}"                    |   false
-const isBalanced = str => {};
+
+//create an empty array
+//create a brackets var with each of the brackets
+//create a for loop that will loop through str.length
+//if the character is an opening brace push it to the stack
+//if the character is a closing brace pop it from the stack
+
+
+
+const isBalanced = str => {
+const stack = [];
+
+const brackets = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+ };
+
+for(let i = 0; i < str.length; i++){
+   if(i ==='(' || i === '[' || i === '{'){
+        stack.push(str[i])
+   } else {
+       const last = stack.pop();
+       if (str[i] !== brackets[last]) {
+           return false
+        };
+   } 
+} 
+    if(stack.length = 0) {
+        return false 
+    };
+    return true;
+
+};
