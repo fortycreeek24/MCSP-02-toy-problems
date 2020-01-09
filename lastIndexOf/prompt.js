8a8b40009852a83d241d8320787b5fe617f5f511
Noah Smith <fortycreeek24@yahoo.com>
2020-01-09 09:56:46 -0600

    adds lastindexof

--- lastIndexOf/prompt.js
+++ lastIndexOf/prompt.js
@@ -1,6 +1,17 @@
 // Write a function that returns the last index that a given element is found
 // Ex: lastIndexOf(4, [2,3,6,4,7,4,9]) returns 5
 
+
+
 function lastIndexOf(target, array) {
-  //solution code here
-}
+  const index = 0;
+    for(i = array.length; i > 0; i--){
+    if(array[i] === target){
+      return index + i
+      }
+    }
+    return index;
+  };
+  
+  const result = lastIndexOf(4, [2,3,6,4,7,4,9]);
+  console.log(result)
